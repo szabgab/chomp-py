dce1a16e14ed98c4576f09c012361d96a3872908
Gabor Szabo <gabor@szabgab.com>
2018-01-11 12:01:59 +0200

    get moves

--- chomp/game.py
+++ chomp/game.py
@@ -7,6 +7,14 @@
 #def play(rows):
 #    if
 
+def get_moves(rows):
+    moves = []
+    for row in range(1, len(rows)+1):
+        for col in range(1, rows[row-1]+1):
+            moves.append((row, col))
+    return moves
+
+
 def get_situation(rows):
     if rows == [1]:
         return 'L'
--- tests/test_chomp.py
+++ tests/test_chomp.py
@@ -3,6 +3,11 @@ import chomp
 def test_chomp():
     assert chomp.game.get_situation([1]) == 'L'
     assert chomp.game.get_situation([1, 1]) == 'W'
+#    assert chomp.game.get_situation([2, 1]) == 'L'
+
     #assert chomp.play([1], "Computer") == 'Computer loses'
     #assert chomp.play([1], "Human") == 'Human loses'
     #assert chomp.play([2], "Computer") == "Take row 1 col 2"
+
+    assert  [(1, 1)] == chomp.game.get_moves([1])
+#    assert chomp.game.get_moves([1, 1]) == [(2, 1)]
